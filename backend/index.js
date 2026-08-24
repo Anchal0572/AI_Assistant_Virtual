92070ed3bc78b3494b87f2b6404d1c94e66bc9a8
Anchal <heerkeshri3@gmail.com>
2026-08-25 01:10:50 +0530

    Setup Express server entry point with middleware and CORS configuration

---
+++ backend/index.js
@@ -0,0 +1,77 @@
+// import express from  "express"
+// import dotenv from "dotenv"
+// dotenv.config()
+// import connectDb from "./config/db.js"
+// import authRouter from "./routes/auth.routes.js"
+// import cors from "cors"
+// import cookieParser from "cookie-parser"
+// import userRouter from "./routes/user.routes.js"
+
+
+
+// const app= express()
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }))
+// const port=process.env.PORT  || 5000
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use("/api/auth",authRouter)
+// app.use("/api/user",userRouter)
+
+
+
+
+// app.listen(port,() =>{
+//     connectDb()
+//     console.log("server started")
+// })
+
+
+
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRouter from "./routes/user.routes.js"
+import geminiResponse from "./gemini.js"
+
+
+const app=express()
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         if (!origin || /^http:\/\/localhost(:\d+)?$/.test(origin)) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     credentials:true
+// }))
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin || /^http:\/\/localhost(:\d+)?$/.test(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true
+}))
+
+const port = process.env.PORT || 8000
+app.use(express.json())
+app.use(cookieParser())
+app.use("/public", express.static("public"))
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
